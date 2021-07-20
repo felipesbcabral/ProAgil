d410d96f4fd8c6f8a91eb387c727f9d29085e0ef
FelipeCabralz <felipesbcabral@gmail.com>
2021-07-20 10:47:46 -0300

    :sparkles: Crud evento

--- ProAgil.Domain/Evento.cs
+++ ProAgil.Domain/Evento.cs
@@ -5,7 +5,7 @@ namespace ProAgil.Domain
 {
     public class Evento
     {
-        public int Id { get; set; }
+        public int Id { get; private set; }
         public string Local { get; set; }
         public DateTime DataEvento { get; set; }
         public string Tema { get; set; }
