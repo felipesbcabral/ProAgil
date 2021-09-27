228364c56c461fa808424749797c012c8cc8394d
Felipe da Silva Barros Cabral <felipe.cabral@corregedoria.df>
2021-09-27 15:47:57 -0300

    Deleting migrations

---
+++ ProAgil.Domain/Identity/Role.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProAgil.Domain.Identity
+{
+    public class Role : IdentityRole<int>
+    {
+        public List<UserRole> UserRoles { get; set; }
+    }
+}
